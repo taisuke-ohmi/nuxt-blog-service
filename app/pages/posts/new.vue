3f9a2fddcb5eb7233608c69969b968db7f6dfe31
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-11-04 20:54:56 +0900

    投稿処理を実装

--- app/pages/posts/new.vue
+++ app/pages/posts/new.vue
@@ -36,8 +36,16 @@ export default {
     ...mapGetters(['user'])
   },
   methods: {
-    publish() {
-    }
+    async publish() {
+      const payload = {
+        user: this.user,
+        ...this.formData,
+      }
+      await this.publishPost({ payload })
+      this.$router.push('/posts')
+    },
+    ...mapActions('users', ['updateUser']),
+    ...mapActions('posts', ['publishPost'])
   }
 }
 </script>
