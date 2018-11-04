b1a517ad8b03be61c58ce1524656bf63d4edf405
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-11-04 21:33:31 +0900

    投稿一覧をfirebaseから取得したデータに

--- app/pages/posts/index.vue
+++ app/pages/posts/index.vue
@@ -17,29 +17,25 @@
 </template>
 
 <script>
+import moment from '~/plugins/moment'
+import { mapGetters } from 'vuex'
+
 export default {
+  async asyncData({ store }) {
+    await store.dispatch('posts/fetchPosts')
+  },
   computed: {
     showPosts() {
-      return [
-        {
-          id: '001',
-          title: 'How to development Nuxt.js Application',
-          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-          created_at: '2018/08/10 12:00:00',
-          user: {
-            id: 'o3tai'
-          }
-        },
-        {
-          id: '002',
-          title: 'Development Nuxt.js Application to Heroku',
-          body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-          created_at: '2018/08/10 13:00:00',
-          user: {
-            id: 'o3tai'
-          }
-        }
-      ]
+      return this.posts.map(post => {
+        post.created_at = moment(post.created_at).format('YYYY/MM/DD HH:mm:ss')
+        return post
+      })
+    },
+    ...mapGetters('posts', ['posts'])
+  },
+  methods: {
+    handleClick(post) {
+      this.$router.push(`/posts/${post.id}`)
     }
   }
 }
