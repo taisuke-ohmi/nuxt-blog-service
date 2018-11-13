a4ba590b5887a9e9b376b8b14a1399a763ee9e3b
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-11-14 00:45:48 +0900

    ssrの対象からいいねボタンを外す

--- app/pages/posts/_id.vue
+++ app/pages/posts/_id.vue
@@ -7,22 +7,24 @@
           <small>by {{post.user.id}}</small>
         </div>
         <p>{{post.body}}</p>
-        <p class="text-right">
-          <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked" @click="unlike" round>
-            <span class="el-icon-star-on"></span>
-            <span>{{post.likes.length}}</span>
-          </el-button>
-          <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
-            <span class="el-icon-star-off"></span>
-            <span>{{post.likes.length}}</span>
-          </el-button>
-        </p>
+        <no-ssr>
+          <p class="text-right">
+            <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked" @click="unlike" round>
+              <span class="el-icon-star-on"></span>
+              <span>{{post.likes.length}}</span>
+            </el-button>
+            <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
+              <span class="el-icon-star-off"></span>
+              <span>{{post.likes.length}}</span>
+            </el-button>
+          </p>
+        </no-ssr>
         <p class="text-right">
           {{post.created_at | time}}
         </p>
       </el-card>
       <p>
-      <nuxt-link to="/posts">&lt; 投稿一覧へ戻る</nuxt-link>
+        <nuxt-link to="/posts">&lt; 投稿一覧へ戻る</nuxt-link>
       </p>
     </div>
   </section>
